c804ba0942b3da7d3f00307797033e635dc5035f
prawamayoza <prawamayoza@gmail.com>
2023-08-13 21:37:06 +0700

    update-guru

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -6,13 +6,15 @@ use CodeIgniter\Model;
 
 class UserModel extends Model
 {
-    // protected $DBGroup          = 'default';
+    protected $DBGroup          = 'default';
     protected $table            = 'users';
     protected $primaryKey       = 'id';
-    // protected $useAutoIncrement = true;
-    // protected $returnType       = 'array';
-    // protected $useSoftDeletes   = false;
-    // protected $protectFields    = true;
-    protected $allowedFields    = ['nik', 'email', 'nama', 'slug', 'jk', 'tanggal_lahir', 'tempat_lahir', 'npwp', 'nip', 'Agama', 'nuptk', 'ukg', 'hp', 'pend_terakhir', 'jurusan', 'jabatan', 'golongan', 'status', 'alamat', 'kec', 'kota', 'provinsi', 'komp_keahlian', 'instansi', 'npsn', 'ajar', 'majar', 'password' ];
-
+    protected $useAutoIncrement = true;
+    protected $returnType       = 'array';
+    protected $useSoftDeletes   = false;
+    protected $protectFields    = true;
+    protected $useTimestamps = true;
+    protected $createdField  = 'created_at';
+    protected $updatedField  = 'updated_at';
+    protected $allowedFields    = ['nik', 'email', 'nama', 'slug', 'jk', 'tanggal_lahir', 'tempat_lahir', 'npwp', 'nip', 'Agama', 'nuptk', 'ukg', 'hp', 'pend_terakhir', 'jurusan', 'jabatan', 'golongan', 'status', 'alamat', 'kec', 'kota', 'provinsi', 'komp_keahlian', 'instansi', 'npsn', 'ajar', 'majar', 'password'];
 }
